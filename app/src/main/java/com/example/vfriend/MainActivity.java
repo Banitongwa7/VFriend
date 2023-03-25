966b97ae896e2f2bcda40f2e6c882fadf4c3b824
Banitongwa David <davidbanitongwa@gmail.com>
2023-03-25 21:43:56 +0100

    add okhttp and API openai

--- app/src/main/java/com/example/vfriend/MainActivity.java
+++ app/src/main/java/com/example/vfriend/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.vfriend;
 
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     ImageButton sendButton;
     List<Message> messageList;
     MessageAdapter messageAdapter;
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
 
 
     @Override
@@ -43,7 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
         sendButton.setOnClickListener(v -> {
             String message = messageEditText.getText().toString().trim();
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            addToChat(message, Message.SENT_BY_ME);
+            messageEditText.setText("");
+            callAPI(message);
+            welcomeTextView.setVisibility(View.GONE);
         });
     }
 
@@ -54,6 +67,58 @@ public class MainActivity extends AppCompatActivity {
                 messageList.add(new Message(message, sendBy));
                 messageAdapter.notifyDataSetChanged();
                 recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
+    void addResponse(String response){
+        //entrain d'ecrire le message
+        messageList.remove(messageList.size()-1);
+
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String message){
+        //entrain d'ecrire le message
+        messageList.add(new Message("...", Message.SENT_BY_BOT));
+
+        //okhttp
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("model","text-davinci-003");
+            jsonObject.put("prompt",message);
+            jsonObject.put("max_tokens","4000");
+            jsonObject.put("temperature","0");
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        RequestBody requestBody = RequestBody.create(JSON, jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization", "Bearer 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")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }else{
+                    addResponse("Erreur : " + response.body().string());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, IOException e) {
+                addResponse("Erreur : " + e.getMessage());
             }
         });
     }
