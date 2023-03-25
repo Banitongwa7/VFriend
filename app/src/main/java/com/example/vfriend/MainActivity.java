0f06beaada837ad06801daec7b19a20f5e9d8171
Banitongwa David <davidbanitongwa@gmail.com>
2023-03-25 19:49:45 +0100

    add layout and add MainActivity class

--- app/src/main/java/com/example/vfriend/MainActivity.java
+++ app/src/main/java/com/example/vfriend/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.vfriend;
 
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcom_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_button);
+
+        sendButton.setOnClickListener(v -> {
+            String message = messageEditText.getText().toString().trim();
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        });
     }
 }
