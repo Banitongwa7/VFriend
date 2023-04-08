a8f71585fff0e5aa993496b09b6cc57fec9421d5
Banitongwa David <davidbanitongwa@gmail.com>
2023-04-08 23:55:22 +0100

    add layout signin and signup

---
+++ app/src/main/java/com/example/vfriend/DatabaseHelper.java
@@ -0,0 +1,9 @@
+package com.example.vfriend;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "vfriend.db";
+    public static final int DATABASE_VERSION = 1;
+
+}
